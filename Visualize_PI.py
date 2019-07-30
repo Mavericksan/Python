6a31f367dfae207b741f96513943c8e1eb3a9261
Mavericksan <hanns-georg.ochsenkuehn@gmx.de>
2019-07-30 22:12:52 +0200

    New File

--- Visualize_PI.py
+++ Visualize_PI.py
@@ -1,7 +1,19 @@
 #visualize PI with 1 mio digits
+#import turtle graphics module
+
+import turtle as tu
+
+lines = 100
 
 with open('1_mio_digits_of_PI.txt','r') as f:
     pi = f.read()
 
 print('PI auf {:,} Stellen'.format(len(pi)))
-print(pi[0],pi[1],'   ',pi[1000000])
+print('Test der ersten 9 Stellen', pi[0:9],'   und die letzten 4 Stellen: ',pi[999_997:1_000_001],'\
+      oder die letzten 10 Stellen: ', pi[-10:])
+print()
+
+for n in range(lines):
+    zahl = int(pi[n])
+    rotation = zahl * 36
+    
