4da2d660555c33a1e4f3f3d12a127252c3ccb56c
Mavericksan <hanns-georg.ochsenkuehn@gmx.de>
2019-08-01 19:59:42 +0200

    Final modification

--- Visualize_PI.py
+++ Visualize_PI.py
@@ -3,7 +3,7 @@
 
 import turtle as tu
 
-lines = 10_000
+#lines = 500_000
 
 with open('1_mio_digits_of_PI.txt','r') as f:
     pi = f.read()
@@ -13,18 +13,24 @@ with open('1_mio_digits_of_PI.txt','r') as f:
 #      oder die letzten 10 Stellen: ', pi[-10:])
 # print()
 
+# set turtle graphics modes
+
 tu.mode('logo')
 tu.tracer(False)
-tu.screensize(5000,5000,'black')
-tu.pencolor('red')
+tu.screensize(12_000,16_000,'black')
+#tu.pencolor('red')
+tu.colormode(255)
 
+# read string and convert single digits to integer for drawing the lines
 
-
+lines=len(pi)
 for n in range(lines):
+    color=int((n/(lines/255)))
     zahl = int(pi[n])
     rotation = zahl * 36
+    tu.pencolor(0+color,255-color,color)
     tu.setheading(rotation)
-    tu.forward(3)
+    tu.forward(8)
 
 tu.done()
 
