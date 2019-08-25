43cda4e4705d21363e89895c0c77e7a50a8123b9
Mavericksan <hanns-georg.ochsenkuehn@gmx.de>
2019-08-25 13:38:52 +0200

    debugged

--- Google_Search_Results.py
+++ Google_Search_Results.py
@@ -7,37 +7,56 @@ from robobrowser import RoboBrowser
 WebSite = 'aps-munich.com'
 
 # Read all predefined tags
-tags_list = ['aps Solutions GmbH', 'aps-munich', 'Everbeing','Mechanical Devices', 'WinWay Technology', 'Test Sockets']
-#tags_list = []
-#with open('aps_tags.txt', 'r') as tags_file:
-#    tags_list = tags_file.read()
-#print(tags_list)
+#tags_list = ['aps Solutions GmbH', 'aps-munich', 'Everbeing','Mechanical Devices', 'WinWay Technology', 'Test Sockets']
+#print(tags_list[0])
+
+tags_list = []
+
+with open('aps_tags.txt', 'r') as tags_file:
+    tags_list_str = tags_file.readline()
+    tags_file.close()
+        
+#print(tags_list_str)
+# String in Liste umwandeln
+tags_list = list(tags_list_str.split(';'))
+
+#for tag in tags_list:
+#    print(tag)
+
+    
 
 now = arrow.now().format('DD.MM.YYYY HH:mm:ss')
-print(now)
+print('\n', now, '\n')
 
-user_agents = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36', 'Mozilla/5.0 (Linux; Android 9; SM-N960F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.111 Mobile Safari/537.36', 'Mozilla/5.0 (Linux; Android 6.0.1; SM-T800) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.111 Safari/537.36']
+#user_agents = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36', 'Mozilla/5.0 (Linux; Android 9; SM-N960F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.111 Mobile Safari/537.36', 'Mozilla/5.0 (Linux; Android 6.0.1; SM-T800) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.111 Safari/537.36']
+#for agents in user_agents:
+#    print(agents)
 
+user_agents = []
+with open('user_agents.txt', 'r') as agents_file:
+    user_agents_str = agents_file.readline()
+#print(user_agents_str)
+#print()
 
-#user_agents = []
-#with open('user_agents.txt', 'r') as agents_file:
-#    user_agents = agents_file.read()
-print(user_agents) 
+#Convert String in List
+user_agents = list(user_agents_str.split('\t'))
+#for agents in user_agents:
+#    print(agents)
+
 
 for tag in tags_list:
     browser = RoboBrowser(history=False, user_agent=random.choice(user_agents), parser='html.parser')
     #print(tag)
-    browser.open('https://www.google.com/search?num=500&q='+ tag)
-    links = browser.find_all('cite', {'class': 'iUH30'})
-    
+    browser.open('https://www.google.de/search?num=500&q='+ tag)
+    links = browser.find_all('cite', {'class': 'iUh30'})
+    #print(links)
     Ranking = 999
     for i, link in enumerate(links):
         if WebSite in str(link):
-            Ranking = i
+            Ranking = i+1
             print(tag+" " + str(Ranking))
             break
     print(f'Ranking: {tag}, {Ranking:03d}')
-
 
 
 
