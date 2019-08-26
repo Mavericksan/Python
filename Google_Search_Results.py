113e59d77cf2eeb06df31ea876dfba6b522b8ac2
Mavericksan <hanns-georg.ochsenkuehn@gmx.de>
2019-08-26 22:51:20 +0200

    staged changes

--- Google_Search_Results.py
+++ Google_Search_Results.py
@@ -1,10 +1,11 @@
 #Google Ranking 
 
+
 import random
 import arrow
 from robobrowser import RoboBrowser
 
-WebSite = 'aps-munich.com'
+WebSite = 'www.aps-munich.com'
 
 # Read all predefined tags
 #tags_list = ['aps Solutions GmbH', 'aps-munich', 'Everbeing','Mechanical Devices', 'WinWay Technology', 'Test Sockets']
@@ -26,7 +27,7 @@ tags_list = list(tags_list_str.split(';'))
     
 
 now = arrow.now().format('DD.MM.YYYY HH:mm:ss')
-print('\n', now, '\n')
+#print('\n', now, '\n')
 
 #user_agents = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36', 'Mozilla/5.0 (Linux; Android 9; SM-N960F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.111 Mobile Safari/537.36', 'Mozilla/5.0 (Linux; Android 6.0.1; SM-T800) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.111 Safari/537.36']
 #for agents in user_agents:
@@ -35,30 +36,34 @@ print('\n', now, '\n')
 user_agents = []
 with open('user_agents.txt', 'r') as agents_file:
     user_agents_str = agents_file.readline()
+    agents_file.close()
 #print(user_agents_str)
 #print()
 
 #Convert String in List
 user_agents = list(user_agents_str.split('\t'))
-#for agents in user_agents:
-#    print(agents)
+for agents in user_agents:
+    print(agents)
 
 
 for tag in tags_list:
     browser = RoboBrowser(history=False, user_agent=random.choice(user_agents), parser='html.parser')
+    #browser = RoboBrowser(history=False, user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36', parser='html.parser')
     #print(tag)
-    browser.open('https://www.google.de/search?num=500&q='+tag)
+    browser.open('https://www.google.de/search?num=500&q='+str(tag))
     links = browser.find_all('cite', {'class': 'iUh30'})
-    print(links)
+    #print(links)
+    with open('links_aps.log', 'a') as links_file:
+        links_file.write(f'{tag}\n {links}\n\n')
+        links_file.close()
     Ranking = 999
     for i, link in enumerate(links):
-        print(str(link))
         if WebSite in str(link):
             Ranking = i+1
-            print(tag+" " + str(Ranking))
+            print(tag+'\t\t' + str(Ranking))
             break
     print(f'Ranking: {tag}, {Ranking:03d}')
-
-
-
-
+    with open('ranking_aps.log','a') as results_file:
+        results_file.write(f'{now};{tag};{Ranking:03d}\n')
+        results_file.close()
+        
